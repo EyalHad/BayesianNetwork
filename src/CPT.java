65274a981f82944f98741a960b2a9ee49e442645
Eyal Hadad <eyalhadad@outlook.com>
2021-11-09 09:41:28 +0200

    "matrix is added & key is changed in _TABLE"

--- src/CPT.java
+++ src/CPT.java
@@ -1,26 +1,35 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class CPT {
 
-    private HashMap<String[],Double> _TABLE;
+    private final HashMap<HashSet<String>,Double> _TABLE;
+    private String[][] matrix;
 
     public CPT(){
         _TABLE = new HashMap<>();
     }
 
-    public void addRow(String[] givens,double probability){
+    public void addRow(HashSet<String> givens,double probability){
         _TABLE.put(givens,probability);
+    }
+
+    public void addMatrix(String[][] matrix){
+        this.matrix = matrix;
+    }
+
+    public String[][] getMatrix() {
+        return matrix;
     }
 
     @Override
     public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String[] s: _TABLE.keySet()) {
-            stringBuilder.append(Arrays.toString(s)).append(" ").append(_TABLE.get(s)).append(", ");
-        }
-        return "CPT:\n"+ stringBuilder.toString()+"\n";
+        return _TABLE.keySet().stream()
+                .map(s -> s.toString() + "->" + _TABLE.get(s) + "\n")
+                .collect(Collectors.joining());
     }
 
 }
