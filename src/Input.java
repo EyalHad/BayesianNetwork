4ae4b4595660aac5ce17c3fa70e844411d6cb01a
Eyal Hadad <eyalhadad@outlook.com>
2021-11-19 01:40:39 +0200

    "continue analyzing the data"

--- src/Input.java
+++ src/Input.java
@@ -1,18 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Input {
 
     private String _XML_filename;
-    private ArrayList<String> rawData = new ArrayList<>();
+    private final ArrayList<String> rawData;
+
+    private String _StartNode;
+    private String _GoalNode;
+    private String[] _Evidence;
 
 
     public Input(String filename){
+            rawData = new ArrayList<>();
             fileRead(filename);
             new XMLParser(_XML_filename);
-
+            mineData();
     }
 
 
@@ -27,6 +33,7 @@ public class Input {
 
                 String data = myReader.nextLine();
                 rawData.add(data);
+
             }
             _XML_filename = rawData.get(0);
             myReader.close();
@@ -36,4 +43,26 @@ public class Input {
             e.printStackTrace();
         }
     }
+
+    private void mineData(){
+        int index = 1;
+        String temp = rawData.get(index);
+
+        while (temp.charAt(1) != '('){
+
+            String[] query = temp.split("\\|");
+            if (query.length > 1){ _Evidence = query[1].split(","); }
+
+            String[] leftSide = query[0].split("-");
+            _StartNode = leftSide[0];
+            _GoalNode = leftSide[1];
+
+            BayesBall bounce = new BayesBall(_StartNode,_GoalNode,_Evidence);
+            Output.addLine(bounce.isIndependent() ? "yes\n" : "no\n");
+
+            temp = rawData.get(++index);
+        }
+    }
+
+
 }
