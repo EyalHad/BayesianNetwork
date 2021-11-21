d6f0c87ec83e8026459bfd192ec92831eddf5233
Eyal Hadad <eyalhadad@outlook.com>
2021-11-21 23:35:13 +0200

    "more readable"

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,11 +1,16 @@
 
 public class Ex1 {
+
     private static final String FILENAME = "input.txt";
 
     public static void main(String[] args) {
 
-       new Input(FILENAME);
-       Output.writeToFile();
+        new Input(FILENAME);
+        Input.readXML();
+        Input.startAlgorithms();
+
+        // The outcome from each algorithm is written to output.txt
+        Output.writeToFile();
     }
 
 }
