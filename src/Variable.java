002826feb648760f11abe24e137f658779169564
Eyal Hadad <eyalhadad@outlook.com>
2021-11-22 00:12:17 +0200

    "added inner class CPT into Variable"

--- src/Variable.java
+++ src/Variable.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Variable {
 
@@ -79,6 +80,37 @@ public class Variable {
         return Objects.equals(name, variable.name) &&
                 Objects.equals(parents, variable.parents) &&
                 Objects.equals(children, variable.children);
+    }
+
+
+    public static class CPT {
+
+        private final HashMap<String, Double> _TABLE;
+        private String[][] matrix;
+
+        public CPT() {
+            _TABLE = new HashMap<>();
+        }
+
+        public void addRow(String givens, double probability) {
+            _TABLE.put(givens, probability);
+        }
+
+        public void addMatrix(String[][] matrix) {
+            this.matrix = matrix;
+        }
+
+        public String[][] getMatrix() {
+            return matrix;
+        }
+
+        @Override
+        public String toString() {
+            return _TABLE.keySet().stream()
+                    .map(s -> s.toString() + "->" + _TABLE.get(s) + "\n")
+                    .collect(Collectors.joining());
+        }
+
     }
 
 
